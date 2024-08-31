d9af8306db27cfa9b74f7125c7e27d884431bdaa
Shelton Ngwenya <shelt15.nn@gmail.com>
2024-08-31 01:00:33 +0100

    removed plugins, updated markdown.vim
    removed :
    - clangd
    - gx
    - makeit

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -4,17 +4,14 @@ return {
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
     build = function()
-      -- Conditionally choose build command
-      -- Assuming some logic or condition to choose between yarn/npm and the default install
-      local use_yarn_or_npm = true -- Example condition, adjust according to your needs
+      local use_yarn_or_npm = true
       if use_yarn_or_npm then
-        vim.fn.system 'cd app && npm install' -- Or replace with the equivalent Lua vim.cmd call
+        vim.fn.system 'cd app && npm install'
       else
         vim.fn['mkdp#util#install']()
       end
     end,
     init = function()
-      -- General settings, using true or false for boolean values
       vim.g.mkdp_auto_start = false
       vim.g.mkdp_auto_close = true
       vim.g.mkdp_refresh_slow = false
@@ -24,8 +21,6 @@ return {
       vim.g.mkdp_browser = 'firefox'
       vim.g.mkdp_echo_preview_url = false
       vim.g.mkdp_browserfunc = ''
-
-      -- Preview options, corrected boolean values
       vim.g.mkdp_preview_options = {
         mkit = {},
         katex = {},
@@ -40,8 +35,6 @@ return {
         disable_filename = false,
         toc = {},
       }
-
-      -- Style configurations
       vim.g.mkdp_markdown_css = ''
       vim.g.mkdp_highlight_css = ''
       vim.g.mkdp_port = ''
@@ -50,27 +43,22 @@ return {
       vim.g.mkdp_theme = 'dark'
       vim.g.mkdp_combine_preview = false
       vim.g.mkdp_combine_preview_auto_refresh = true
-      -- Dynamic image path setup
       vim.api.nvim_create_autocmd('BufReadPre', {
         pattern = '*.md',
         callback = function()
-          -- Define 'sep' in the outer scope so it can be used throughout the callback
-          local sep = package.config:sub(1, 1) -- System's file separator (e.g., '/' for Unix)
-
-          -- Function to find the "notes" directory from the current file path
+          local sep = package.config:sub(1, 1)
           local function find_notes_dir(path)
             local parts = {}
             for part in string.gmatch(path, '[^' .. sep .. ']+') do
               table.insert(parts, part)
-              if part == 'Notes' then
-                print("Found 'notes' directory in path: " .. table.concat(parts, sep)) -- Debug print
+              if part:lower() == 'notes' then
                 break
               end
             end
             return table.concat(parts, sep)
           end
 
-          local current_path = vim.fn.expand '%:p' -- Get the full path of the current file
+          local current_path = vim.fn.expand '%:p'
           local notes_dir = find_notes_dir(current_path)
           if notes_dir ~= '' then
             local images_path = notes_dir .. sep .. 'assets' .. sep .. 'imgs'
@@ -80,76 +68,156 @@ return {
       })
     end,
   },
-  -- Install the markdown-toc.nvim plugin
   {
     'hedyhli/markdown-toc.nvim',
-    ft = 'markdown', -- Lazy load on markdown filetype
-    cmd = { 'Mtoc' }, -- Or, lazy load on "Mtoc" command
+    ft = 'markdown',
+    cmd = { 'Mtoc' },
     opts = {
-      -- Enable auto-update of the ToC on buffer save
       auto_update = {
         enabled = true,
         events = { 'BufWritePre' },
         pattern = '*.{md,mdown,mkd,mkdn,markdown,mdwn}',
       },
-      -- Headings configuration
       headings = {
-        before_toc = false, -- Do not include headings before the ToC
-        exclude = { 'CHANGELOG', 'License' }, -- Exclude specific headings
+        before_toc = false,
+        exclude = { 'CHANGELOG', 'License' },
       },
-      -- ToC list configuration
       toc_list = {
-        markers = '*', -- Use '*' for list items
-        cycle_markers = false, -- Disable marker cycling
+        markers = '*',
+        cycle_markers = false,
         indent_size = function()
-          return vim.bo.shiftwidth -- Set indent size based on shiftwidth
+          return vim.bo.shiftwidth
         end,
-        item_format_string = '${indent}${marker} [${name}](#${link})', -- Format for ToC items
+        item_format_string = '${indent}${marker} [${name}](#${link})',
         item_formatter = function(item, fmtstr)
-          -- Customize the link format
           local s = fmtstr:gsub([[${(%w-)}]], function(key)
             return item[key] or ('${' .. key .. '}')
           end)
           return s
         end,
       },
-      -- Fences configuration
       fences = {
-        enabled = true, -- Enable fences
-        start_text = 'mtoc-start', -- Start text for fences
-        end_text = 'mtoc-end', -- End text for fences
+        enabled = true,
+        start_text = 'mtoc-start',
+        end_text = 'mtoc-end',
       },
     },
     config = function(_, opts)
-      -- Setup the plugin with the given options
       require('mtoc').setup(opts)
     end,
   },
   {
-    'lukas-reineke/headlines.nvim',
-    opts = function()
-      local opts = {}
-      for _, ft in ipairs { 'markdown', 'norg', 'rmd', 'org' } do
-        opts[ft] = {
-          headline_highlights = {},
-          -- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
-          bullets = {},
-        }
-        for i = 1, 6 do
-          local hl = 'Headline' .. i
-          vim.api.nvim_set_hl(0, hl, { link = 'Headline', default = true })
-          table.insert(opts[ft].headline_highlights, hl)
-        end
-      end
-      return opts
-    end,
-    ft = { 'markdown', 'norg', 'rmd', 'org' },
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      heading = {
+        enabled = true,
+        sign = true,
+        position = 'overlay',
+        icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+        signs = { '󰫎 ' },
+        width = 'full',
+        left_pad = 0,
+        right_pad = 0,
+        min_width = 0,
+        border = false,
+        border_prefix = false,
+        above = '▄',
+        below = '▀',
+        backgrounds = {
+          'RenderMarkdownH1Bg',
+          'RenderMarkdownH2Bg',
+          'RenderMarkdownH3Bg',
+          'RenderMarkdownH4Bg',
+          'RenderMarkdownH5Bg',
+          'RenderMarkdownH6Bg',
+        },
+        foregrounds = {
+          'RenderMarkdownH1',
+          'RenderMarkdownH2',
+          'RenderMarkdownH3',
+          'RenderMarkdownH4',
+          'RenderMarkdownH5',
+          'RenderMarkdownH6',
+        },
+      },
+      code = {
+        enabled = true,
+        sign = true,
+        style = 'full',
+        position = 'left',
+        language_pad = 0,
+        disable_background = { 'diff' },
+        width = 'full',
+        left_pad = 0,
+        right_pad = 0,
+        min_width = 0,
+        border = 'thin',
+        above = '▄',
+        below = '▀',
+        highlight = 'RenderMarkdownCode',
+        highlight_inline = 'RenderMarkdownCodeInline',
+      },
+      dash = {
+        enabled = true,
+        icon = '─',
+        width = 'full',
+        highlight = 'RenderMarkdownDash',
+      },
+      bullet = {
+        enabled = true,
+        icons = { '●', '○', '◆', '◇' },
+        left_pad = 0,
+        right_pad = 0,
+        highlight = 'RenderMarkdownBullet',
+      },
+      checkbox = {
+        enabled = true,
+        position = 'inline',
+        unchecked = {
+          icon = '󰄱 ',
+          highlight = 'RenderMarkdownUnchecked',
+        },
+        checked = {
+          icon = '󰱒 ',
+          highlight = 'RenderMarkdownChecked',
+        },
+        custom = {
+          todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'RenderMarkdownTodo' },
+        },
+      },
+      quote = {
+        enabled = true,
+        icon = '▋',
+        repeat_linebreak = false,
+        highlight = 'RenderMarkdownQuote',
+      },
+      pipe_table = {
+        enabled = true,
+        preset = 'none',
+        style = 'full',
+        cell = 'padded',
+        alignment_indicator = '━',
+        border = {
+          '┌',
+          '┬',
+          '┐',
+          '├',
+          '┼',
+          '┤',
+          '└',
+          '┴',
+          '┘',
+          '│',
+          '─',
+        },
+        head = 'RenderMarkdownTableHead',
+        row = 'RenderMarkdownTableRow',
+        filler = 'RenderMarkdownTableFill',
+      },
+    },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim', 'nvim-tree/nvim-web-devicons' },
     config = function(_, opts)
-      -- PERF: schedule to prevent headlines slowing down opening a file
-      vim.schedule(function()
-        require('headlines').setup(opts)
-        require('headlines').refresh()
-      end)
+      require('render-markdown').setup(opts)
     end,
   },
 }
