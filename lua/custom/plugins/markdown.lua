84116e59406735c621179cd6cf716f248aa0ba8f
Shelton Ngwenya <shelt15.nn@gmail.com>
2025-02-25 09:22:28 +0000

    added peeknvim, markdown preview doesnt work on wsl2

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -69,6 +69,38 @@ return {
     end,
   },
   {
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+      local peek = require 'peek'
+
+      peek.setup {
+        auto_load = true,
+        close_on_bdelete = true,
+        syntax = true,
+        theme = 'dark',
+        update_on_change = true,
+        app = 'webview',
+        filetype = { 'markdown' },
+      }
+
+      vim.api.nvim_create_user_command('PeekOpen', function()
+        if not peek.is_open() and vim.bo[vim.api.nvim_get_current_buf()].filetype == 'markdown' then
+          vim.fn.system 'i3-msg split horizontal'
+          peek.open()
+        end
+      end, {})
+
+      vim.api.nvim_create_user_command('PeekClose', function()
+        if peek.is_open() then
+          peek.close()
+          vim.fn.system 'i3-msg move left'
+        end
+      end, {})
+    end,
+  },
+  {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {
       heading = {
